149e8deff477364bfb5cb0ccb50cb77629d60fb5
NotDifined <kakak.jiddan@gmail.com>
2026-02-16 06:38:51 +0700

    0.2

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-from fastapi.middleware.wsgi import WSGIMiddleware
 from flask import Flask, render_template
 from flask_cors import CORS
 
-# from time import sleep
 from aiAgent import input_req
 from SU import search_web
 from until.response import response
 
-
-# ini untuk start pas app awal dimulai dan dimatiin
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-
-
 app = Flask(__name__)
-fast_app = FastAPI()
 CORS(app)
 
 
@@ -33,20 +20,16 @@ def mikir(target):
         return response(500, str(e), "error")
 
 
-@fast_app.get("/")
+@app.get("/")
 def index():
     return render_template("index.html")
 
 
-@fast_app.get("/api/search-user/{target}")
+@app.get("/api/search-user/{target}")
 def search(target: str):
     return search_web(target, True)
 
 
-@fast_app.get("/api/")
+@app.get("/api/")
 def health():
     return {"status": "ok"}
-
-
-# flapp pakai middleware WSGI trs di mount ke fast_app
-fast_app.mount("/", WSGIMiddleware(app))
